f64159b6eadb7a0d1112eafbf5bf964f1cc08ffc
Christian Raphael <christianraphael.1309@gmail.com>
2026-02-23 20:54:17 -0300

    add status and languages sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import React from "react"
+import { languages } from "./languages"
+
 export default function AssemblyEndgame() {
+  const languageElements = languages.map(lang => {
+    const styles={
+      backgroundColor: lang.backgroundColor, 
+      color: lang.color
+    }
+    return (<span key={lang.name} className="chip" style={styles}>{lang.name}</span>)
+  })
 
   return (
     <main>
@@ -6,6 +16,15 @@ export default function AssemblyEndgame() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+
+      <section className="game-status">
+        <h2>You Win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+
+      <section className="language-chips">
+        {languageElements}
+      </section>
     </main>
   )
 }
