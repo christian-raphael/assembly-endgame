a5222a97cf59dddf68c87204c400663c419b7adc
Christian Raphael <christianraphael.1309@gmail.com>
2026-02-27 20:10:36 -0300

    add a wrong guesses count

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,89 @@
-import React from "react"
-import { languages } from "./languages"
+import { useState } from "react"
 import { clsx } from "clsx"
+import { languages } from "./languages"
 
 export default function AssemblyEndgame() {
-  const [currentWord, setCurrentWord] = React.useState("react")
-  const [guessedLetters, setGuessedLetters] = React.useState([])
+    const [currentWord, setCurrentWord] = useState("react")
+    const [guessedLetters, setGuessedLetters] = useState([])
 
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    const wrongGuessCount = 
+        guessedLetters.filter(letter => !currentWord.includes(letter)).length
 
-  const languageElements = languages.map(lang => {
-    const styles={
-      backgroundColor: lang.backgroundColor, 
-      color: lang.color
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    function addGuessedLetter(letter) {
+        setGuessedLetters(prevLetters =>
+            prevLetters.includes(letter) ?
+                prevLetters :
+                [...prevLetters, letter]
+        )
     }
-    return (<span key={lang.name} className="chip" style={styles}>{lang.name}</span>)
-  })
 
-  const letterElements = currentWord.split('').map((letter, index) => (
-    <span key={index}>
-      {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
-    </span>
-  ))
+    const languageElements = languages.map((lang, index) => {
+        const isLanguageLost = index < wrongGuessCount
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        const className = clsx("chip", isLanguageLost && "lost")
+        return (
+            <span
+                className={className}
+                style={styles}
+                key={lang.name}
+            >
+                {lang.name}
+            </span>
+        )
+    })
 
-  function addGuessedLetter(letter) {
-    setGuessedLetters(prevLetters => 
-      prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]
-    )
-  }
+    const letterElements = currentWord.split("").map((letter, index) => (
+        <span key={index}>
+            {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+        </span>
+    ))
 
-  const keyboardElements = alphabet.split('').map(letter => {
-    const isGuessed = guessedLetters.includes(letter);
-    const isCorrect = isGuessed && currentWord.includes(letter);
-    const isWrong = isGuessed && !currentWord.includes(letter);
-    const className = clsx({
-      correct: isCorrect,
-      wrong: isWrong
+    const keyboardElements = alphabet.split("").map(letter => {
+        const isGuessed = guessedLetters.includes(letter)
+        const isCorrect = isGuessed && currentWord.includes(letter)
+        const isWrong = isGuessed && !currentWord.includes(letter)
+        const className = clsx({
+            correct: isCorrect,
+            wrong: isWrong
+        })
+        
+        return (
+            <button
+                className={className}
+                key={letter}
+                onClick={() => addGuessedLetter(letter)}
+            >
+                {letter.toUpperCase()}
+            </button>
+        )
     })
 
     return (
-      <button 
-        key={letter} 
-        onClick={() => {addGuessedLetter(letter)}}
-        className={className}
-      >
-          {letter.toUpperCase()}
-      </button>
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+            </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
+            <section className="language-chips">
+                {languageElements}
+            </section>
+            <section className="word">
+                {letterElements}
+            </section>
+            <section className="keyboard">
+                {keyboardElements}
+            </section>
+            <button className="new-game">New Game</button>
+        </main>
     )
-  })
-
-  return (
-    <main>
-      <header>
-        <h1>Assembly: Endgame</h1>
-        <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
-      </header>
-
-      <section className="game-status">
-        <h2>You Win!</h2>
-        <p>Well done! 🎉</p>
-      </section>
-
-      <section className="language-chips">
-        {languageElements}
-      </section>
-      
-      <section className="word">
-        {letterElements}
-      </section>
-
-      <section className="keyboard">
-        {keyboardElements}
-      </section>
-    </main>
-  )
 }
